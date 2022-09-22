68b81bca5a7e80106a73e18fac86574020436a2e
Kirill Loskutov <kl@sinker.ru>
2022-09-22 13:07:15 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { bot } from "./bot.ts";
 const handleUpdate = webhookCallback(bot, "std/http");
 
 serve(async (req) => {
-  if (req.method == "POST") {
+  if (req.method === "POST") {
     const url = new URL(req.url);
     if (url.pathname.slice(1) === bot.token) {
       try {
