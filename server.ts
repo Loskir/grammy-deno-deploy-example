b1060df57724947908e306288926e3a08e5e885d
Kirill Loskutov <kl@sinker.ru>
2022-09-22 13:06:20 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,21 +1,19 @@
-import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
+import { webhookCallback } from "https://deno.land/x/grammy@v1.11.0/mod.ts";
 import { bot } from "./bot.ts";
-import { webhookCallback } from "./deps.deno.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-serve({
-  ["/" + Deno.env.get("TOKEN")]: async (req) => {
-    if (req.method == "POST") {
+serve(async (req) => {
+  if (req.method == "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
       try {
         return await handleUpdate(req);
       } catch (err) {
         console.error(err);
       }
     }
-    return new Response();
-  },
-  "/": () => {
-    return new Response("Hello world!");
-  },
+  }
+  return new Response();
 });
